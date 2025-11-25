ec8367d527e6482c5a0132702753f91c17666471
Salinda Kumara <salindakumara02@gmail.com>
2025-11-25 09:51:37 +0530

    lms button

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,19 +12,6 @@ export default function Header() {
                         Document Submission Status Portal
                     </p>
                 </div>
-
-                <a
-                    href="https://sablms.casrilanka.com"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="header-link"
-                >
-                    <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-                        <path d="M12 2L2 7l10 5 10-5-10-5z" />
-                        <path d="M2 17l10 5 10-5M2 12l10 5 10-5" />
-                    </svg>
-                    SAB LMS
-                </a>
             </div>
         </header>
     );
