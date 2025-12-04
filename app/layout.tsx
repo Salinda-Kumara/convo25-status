3264cbe85ec1456cee857a35f1f6bddf0bef73e3
Salinda Kumara <salindakumara02@gmail.com>
2025-12-04 08:50:05 +0530

    vulnerability-fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -29,7 +28,6 @@ export default function RootLayout({
           {children}
         </div>
         <Footer />
-        <Analytics />
       </body>
     </html>
   );
